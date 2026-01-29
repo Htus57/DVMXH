92cd249a5154d5c09b3dfe120b2ab741c0495812
Htus57 <lehoangtu0608@gmail.com>
2026-01-29 17:58:23 +0900

    feat: Consolidate all files into a single index.html

--- js/detail.js
+++
@@ -1,4 +0,0 @@
-const urlParams = new URLSearchParams(window.location.search);
-const itemId = urlParams.get('itemId');
-
-document.getElementById('content').innerText = `Details for item ${itemId}`;
